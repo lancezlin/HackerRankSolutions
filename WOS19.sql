f38059d42f5bf475b2ca04f0fdf265a9ad337989
Lance Lin <linzhengzhong@icloud.com>
2015-10-13 22:58:45 -0700

    Create WOS19.sql

---
+++ WOS19.sql
@@ -0,0 +1,21 @@
+/*
+Given a table STATION that holds data for five fields namely ID, CITY, STATE, NORTHERN LATITUDE and WESTERN LONGITUDE.
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+ 
+Consider P1(a, b) and P2(c, d) be two points on 2D plane, where (a, b) be minimum and maximum values of Northern Latitude and (c, d) be minimum and maximum values of Western Longitude. Write a query to print the Euclidean Distance between points P1 and P2 up to 4 decimal digits.
+*/
+SELECT ROUND(SQRT(POW((a-c),2)+POW((b-d),2)), 4) AS md
+FROM
+(SELECT ROUND(MIN(LAT_N), 4) AS a, ROUND(MAX(LAT_N), 4) AS b,
+    ROUND(MIN(LONG_W), 4) AS c, ROUND(MAX(LONG_W), 4) AS d
+FROM STATION) AS T
+;
